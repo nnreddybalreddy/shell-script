6f6b0a3a2038f6daaaa7908ca56023fb01c2001e
nnreddybalreddy <narendra.mahn@gmail.com>
2025-02-15 18:08:03 -0500

    ALL

--- 11-func.sh
+++ 11-func.sh
@@ -2,6 +2,16 @@
 
 USERID=$(id -u)
 
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then 
+        echo "$2...failure"
+        exit 1
+    else 
+        echo "$2...PASS" 
+    fi
+}
+
 if [ $USERID -ne 0 ]
 then
     echo "Not a super user"
@@ -12,22 +22,13 @@ fi
 
 dnf install mysql -y
 
-if [ $? -ne 0 ]
-then 
-    echo "mysql installation fail"
-    exit
-else 
-    echo "Installation of mysql failed"
-fi
+VALIDATE $? "Mysql installation"
+
 
 dnf install git -y 
 
-if [ $? -ne 0 ]
-then 
-    echo "git Installation fail"
-    exit 
-else 
-    echo "Installation of git passed"
-fi         
+VALIDATE $? "git installation"
+
+       
 
 
