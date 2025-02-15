8915257d1d1844c95219159309331c9e9325325e
nnreddybalreddy <narendra.mahn@gmail.com>
2025-02-15 06:52:38 -0500

    ALL

--- 02-var1.sh
+++ 02-var1.sh
@@ -1,13 +1,23 @@
 #!/bin/bash
 
-echo "all:::$@"
-echo "No : $#"
-echo "script  name: $0"
-echo "$PWD"
-echo "$USER"
-echo "$HOSTNAME"
-echo "$HOME"
+# echo "all:::$@"
+# echo "No : $#"
+# echo "script  name: $0"
+# echo "$PWD"
+# echo "$USER"
+# echo "$HOSTNAME"
+# echo "$HOME"
 
-echo "pid: $$"
-sleep 60 &
-echo "backgroud pid:$!"
+# echo "pid: $$"
+# sleep 60 &
+# echo "backgroud pid:$!"
+
+NUM1=10
+
+if [ $NUM -ge 10 ]
+then
+    echo "No ge 10"
+else
+    echo "ne le 10"
+fi        
+
