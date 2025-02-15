658a1fe214fc7d2beae69ecf091f45c74d620feb
nnreddybalreddy <narendra.mahn@gmail.com>
2025-02-15 18:59:21 -0500

    ALL

---
+++ 12-colors.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+TIMESTAMP$(date +%F-%M-%H-%S)
+SCRIPT_NAME=$(echo $0 | cut -f "." -f1)
+
+LOGFILE=/tmp/$TIMESTAMP-$SCRIPT_NAME.log
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then 
+        echo "$2... $R failure $N"
+        exit 1
+    else 
+        echo "$2... $G pass $N"    
+
+    fi
+}
+USERID=$(id -u)
+
+if [ $USERID -ne 0 ]
+then 
+    echo "not a root user"
+    exit 1
+else 
+    echo "Its a root user"    
+fi
+
+
+
+dnf install mysql -y &>>LOGFILE
+VALIDATE $? "mysql instalation"
+
+dnf install git -y >>&LOGFILE
+VALIDATE $? "git installation"
