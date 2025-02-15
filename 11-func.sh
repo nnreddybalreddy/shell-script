56d4d9bc346cc41a034af252c861503e41e27204
nnreddybalreddy <narendra.mahn@gmail.com>
2025-02-15 13:19:51 -0500

    ALL

---
+++ 11-func.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+if [ $USERID -ne 0 ]
+then
+    echo "Not a super user"
+    exit 1
+else
+    echo "Super User"
+
+
+dnf install mysqld -y 
+
+if [ $? -ne 0 ]
+then 
+    echo "git installation failed"
+    exit 1
+else 
+    echo "git Installation success"    
+
+fi
+
+dnf install git -y 
+
+if [ $? -ne 0 ]
+then 
+    echo "git installation failed"
+    exit 1
+else 
+    echo "git Installation success"    
+
+fi
