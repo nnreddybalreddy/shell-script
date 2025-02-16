5d0b75f2dddf383b7eeaa9f108ba719a0e399321
nnreddybalreddy <narendra.mahn@gmail.com>
2025-02-16 07:34:50 -0500

    ALL

--- 10-installations.sh
+++ 10-installations.sh
@@ -68,7 +68,7 @@ do
     dnf list installed $i &>>$LOGFILE
     if [ $? -eq 0 ]
     then 
-        echo -e "$i already installed"
+        echo -e "$i $Y already installed $N"
     else 
         dnf install $i -y &>>$LOGFILE
         VALIDATE $? "$i installation"  
