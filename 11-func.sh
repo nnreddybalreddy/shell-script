46b9c8bdeb798d3d3b0ea47b0efe891439eda3a8
nnreddybalreddy <narendra.mahn@gmail.com>
2025-02-16 00:05:16 -0500

    ALL

--- 11-func.sh
+++ 11-func.sh
@@ -33,6 +33,10 @@
 
 # VALIDATE $? "git installation"
 
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+
 TIMESTAMP=$(date +%F-%H-%M-%S)
 SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
 
@@ -41,10 +45,10 @@ LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
 VALIDATE(){
     if [ $1 -ne 0 ]
     then 
-        echo "$2  failed...."
+        echo "$2  $R failed....$N"
         exit 1
     else
-        echo "$2  pass..."    
+        echo "$2  $G pass...$N"    
     fi
 }
 
