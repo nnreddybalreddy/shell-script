2e2e0e8ab6aa8fcf1aaaf4245780971ebeb47717
nnreddybalreddy <narendra.mahn@gmail.com>
2025-02-21 21:43:39 -0500

    ALL

--- 15-current.sh
+++ 15-current.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
 
+# COURSE="DEVOPS"
+# echo "Course in current scirpt:$COURSE"
+# echo "PID of current script:$$"
+
+
+# source ./16-other.sh 
+# echo "Course after other script:$COURSE"
+
 COURSE="DEVOPS"
-echo "Course in current scirpt:$COURSE"
+echo "current: $COURSE"
 echo "PID of current script:$$"
 
 
-source ./16-other.sh 
+./16-other.sh 
+
 echo "Course after other script:$COURSE"
