a4f33ade185bb38bb90a5332d0f4f3cee8864b3a
nnreddybalreddy <narendra.mahn@gmail.com>
2025-02-22 08:12:51 -0500

    ALL

--- 19-diskusage.sh
+++ 19-diskusage.sh
@@ -7,10 +7,7 @@ while IFS= read -r line
 do
     USAGE=$(echo $line | awk -F " " '{print $6F}' | cut -d "%" -f1)
     FOLDER=$(echo $line | awk -F " " '{print $NF}')
-    if [ $line -ge $THRESHOLD ]
-    then 
-        echo "$line is greater than $THRSHOLD" 
-    fi 
+    echo $line
 
 done<<< $DISK_USAGE
 
